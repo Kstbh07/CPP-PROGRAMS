6b8bca9cc7874376805107d3684232a1b1d11c42
Kaustubh Sen <kaustubhsen2707@gmail.com>
2026-06-23 20:19:53 +0530

    Time: 24 ms (27.97%), Space: 28.2 MB (83.12%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string st;
+        for(char ch : s) {
+            if(ch == '*') {
+                st.pop_back();
+            } else {
+                st.push_back(ch);
+            }
+        }
+        return st;
+
+    }
+};
