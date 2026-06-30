0e0e84e702565913a8ac40860f0305fc94777f1c
Kaustubh Sen <kaustubhsen2707@gmail.com>
2026-06-30 13:10:49 +0530

    Time: 0 ms (100%), Space: 10.7 MB (61.83%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void backtrack(string &s, int idx, int parts, string curr) {
+        if (parts == 4 && idx == s.size()) {
+            curr.pop_back();          // remove last '.'
+            ans.push_back(curr);
+            return;
+        }
+
+        if (parts == 4 || idx == s.size())
+            return;
+
+        for (int len = 1; len <= 3 && idx + len <= s.size(); len++) {
+            string part = s.substr(idx, len);
+
+            // Leading zero
+            if (part.size() > 1 && part[0] == '0')
+                break;
+
+            // Greater than 255
+            if (stoi(part) > 255)
+                continue;
+
+            backtrack(s, idx + len, parts + 1, curr + part + ".");
+        }
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        if (s.size() < 4 || s.size() > 12)
+            return {};
+
+        backtrack(s, 0, 0, "");
+        return ans;
+    }
+};
