482b716e54139a3a3c9bdccbdf8e3eb59cf01077
Kaustubh Sen <kaustubhsen2707@gmail.com>
2026-07-23 10:47:06 +0530

    Time: 0 ms (100%), Space: 17.7 MB (20.2%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
-    int firstOccurrence(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
+    int first(vector<int>& nums, int target) {
+        int st = 0, end=nums.size()-1;
+        int ans =-1;
+        while(st<=end) {
+            int mid = st+(end-st)/2;
+            if(nums[mid] == target)
+            {
                 ans = mid;
-                high = mid - 1;  
+                end=mid-1;
             }
-            else if (nums[mid] < target) {
-                low = mid + 1;
+            else if(nums[mid] < target)
+            {
+                st = mid+1;
             }
-            else {
-                high = mid - 1;
+            else
+            {
+                end = mid-1;
             }
         }
         return ans;
     }
-
-    int lastOccurrence(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
+    int last(vector<int>& nums, int target) {
+        int st = 0, end=nums.size()-1;
+        int ans =-1;
+        while(st<=end) {
+            int mid = st+(end-st)/2;
+            if(nums[mid] == target)
+            {
                 ans = mid;
-                low = mid + 1;   
+                st=mid+1;
             }
-            else if (nums[mid] < target) {
-                low = mid + 1;
+            else if(nums[mid] < target)
+            {
+                st = mid+1;
             }
-            else {
-                high = mid - 1;
+            else
+            {
+                end = mid-1;
             }
         }
         return ans;
     }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {firstOccurrence(nums, target),
-                lastOccurrence(nums, target)};
+        int firstidx = first(nums,target);
+        int lastidx = last(nums,target);
+        return {firstidx, lastidx};
     }
 };
