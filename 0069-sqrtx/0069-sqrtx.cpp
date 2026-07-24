9508b8bde27bb3424d87901f03e5f9e7c837769d
Kaustubh Sen <kaustubhsen2707@gmail.com>
2026-07-24 11:33:01 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.67%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int ans = pow(x,0.5);
+        int lw = 0, hi = x;
+        int ans = 0;
+        while(lw<=hi) {
+            int mid = lw+(hi-lw)/2;
+            long long sq = 1LL *mid*mid;
+            if(sq == x) {
+                ans = mid;
+                return mid;
+            }
+            else if(sq<x) {
+                ans = mid;
+                lw=mid+1;
+            }
+            else {
+                hi = mid-1;
+            }
+        }
         return ans;
     }
 };
