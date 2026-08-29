81224aea987813cc4cd9427b3179c679667e7889
Kaustubh Sen <kaustubhsen2707@gmail.com>
2026-08-29 23:17:17 +0530

    Time: 0 ms (100%), Space: 30.9 MB (92.35%) - LeetHub

---
+++ 4020-elevator-requests-i/4020-elevator-requests-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int elevatorRequests(int n, vector<int>& requests) {
+        int prev = 0, ans = 0;
+        for (int i = 0; i < requests.size(); i++) {
+            ans += abs(prev - requests[i]);
+            prev = requests[i];
+        }
+        return ans;
+    }
+};
